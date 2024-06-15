dde26eaceb819880a1956e43fb02b4fa022aee0f
raychelleKalekye <kalekye.raychelle@strathmore.edu>
2024-06-15 17:57:21 +0300

    added clubs and sports module

---
+++ routes/Clubs.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const connection = require('../database');
+const router = express.Router();
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+router.get('/', (req, res) => {
+    var query = "SELECT * FROM clubsandsocieties";
+    connection.query(query, function(error, data) {
+        if (error) {
+            console.error(error);
+        } else {
+            res.render('Clubs', { title: 'StrathBud CLUBS AND SOCIETIES', action: 'list', clubsData: data });
+        }
+    });
+});
+
+router.get('/newClub', (req, res) => {
+    res.render('Clubs', { title: 'StrathBud Clubs', action: 'add' });
+});
+router.post('/newClub', (req, res) => {
+    var clubNo = req.body.clubNo;
+    var clubName = req.body.clubName;
+    var clubLeader = req.body.clubLeader;
+    var meetupDays = req.body.meetupDays;
+    var description = req.body.description;
+    var email=req.body.Email;
+    var Location = req.body.Location;
+
+    var query = `INSERT INTO clubsandsocieties (clubNo, clubName, clubLeader, meetupDays, description, Location,email) VALUES (?,?, ?, ?, ?, ?, ?)`;
+
+    connection.query(query, [clubNo, clubName, clubLeader, meetupDays, description, Location,email], function (error, results) {
+        if (error) {
+            console.error(error);
+            return res.status(500).send("Error adding club");
+        } else {
+            res.send('<p>CLUB ADDED SUCCESSFULLY</p>');
+        }
+    });
+});
+
+router.get('/:clubNo', (req, res) => {
+    const clubNo = req.params.clubNo;
+    const query = "SELECT * FROM clubsandsocieties WHERE clubNo = ?";
+    connection.query(query, [clubNo], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Server error');
+        } else if (results.length === 0) {
+            res.status(404).send('Club not found');
+        } else {
+            const clubsData = results[0];
+            console.log('Club Data:', clubsData);  
+            res.render('clubDetails', { clubsData });
+        }
+    });
+});
+
+module.exports = router;
+
+
+
--- routes/Sports.js
+++ routes/Sports.js
@@ -1,18 +1,65 @@
-const express=require('express')
-const connection=require('../database')
-const router=express.Router()
-const multer=require('multer')
+const express = require('express');
+const connection = require('../database');
+const router = express.Router();
+const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.get('/',(req,res)=>{
-    var query="SELECT sportsNo,sportsName,practiceDays,Location FROM sports";
-    connection.query(query,function(error,data){
-        if(error){
-            console.error(error)
-        }else{
+router.get('/', (req, res) => {
+    var query = "SELECT * FROM sports";
+    connection.query(query, function(error, data) {
+        if (error) {
+            console.error(error);
+        } else {
             res.render('Sports', { title: 'StrathBud Sports', action: 'list', sportsData: data });
         }
-    })
-    
-})
+    });
+});
+
+router.get('/newSport', (req, res) => {
+    res.render('Sports', { title: 'StrathBud Sports', action: 'add' });
+});
+router.post('/newSport', (req, res) => {
+    var sportsNo = req.body.sportsNo;
+    var sportsName = req.body.sportsName;
+    var sportsLeader = req.body.sportsLeader;
+    var practiceDays = req.body.practiceDays;
+    var registrationLink = req.body.Registration_link;
+    var Location = req.body.Location;
+
+    var query = `INSERT INTO sports (sportsNo, sportsName, sportsLeader, practiceDays, Registration_link, Location) VALUES (?, ?, ?, ?, ?, ?)`;
+
+    connection.query(query, [sportsNo, sportsName, sportsLeader, practiceDays, registrationLink, Location], function (error, results) {
+        if (error) {
+            console.error(error);
+            return res.status(500).send("Error adding sports");
+        } else {
+            res.send('<p>SPORT ADDED SUCCESSFULLY</p>');
+        }
+    });
+});
+
+
+
+
+
+
+
+router.get('/:sportsNo', (req, res) => {
+    const sportsNo = req.params.sportsNo;
+    const query = "SELECT * FROM sports WHERE sportsNo = ?";
+    connection.query(query, [sportsNo], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Server error');
+        } else if (results.length === 0) {
+            res.status(404).send('Sport not found');
+        } else {
+            const sportsData = results[0];
+            console.log('Sports Data:', sportsData);  
+            res.render('sportsDetails', { sportsData });
+        }
+    });
+});
+
+module.exports = router;
